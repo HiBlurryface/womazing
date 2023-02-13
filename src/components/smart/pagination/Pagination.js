dc78839d1fa05bce93346eddc00e6ce75dc12f4e
Alexandr Ksenz <AlexandrKsenz@gmail.com>
2023-02-14 00:17:47 +0100

    added adaptive & dynamic pagination

--- src/components/smart/pagination/Pagination.js
+++ src/components/smart/pagination/Pagination.js
@@ -1,36 +1,60 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import classNames from "classnames";
 
 import buttonNext from './../../../assets/images/slider-arrow-next.svg'
 import buttonPrev from './../../../assets/images/slider-arrow-prev.svg'
 import styles from './Pagination.module.scss'
 function Pagination({ itemsPerPage, items, currentPage, setCurrentPage }) {
-    const pageNumbers = [];
+    const [currentButton, setCurrentButton] = useState(1);
+    const [currentPageNumber, setCurrentPageNumber] = useState([])
+    const pageNumber = [];
     for (let i = 1; i <= Math.ceil(items / itemsPerPage); i++) {
-        pageNumbers.push(i)
+        pageNumber.push(i)
     }
+
+    useEffect(() => {
+        setCurrentButton(1)
+    }, [items, itemsPerPage])
+
+    useEffect(() => {
+        let pageNumberList = [...currentPageNumber];
+        pageNumberList = pageNumber.slice(0, 5)
+
+        if (currentButton <= 3) {
+            pageNumberList = pageNumber.slice(0, 5)
+        } else if (pageNumber.length != 4 && currentButton > 3 && currentButton < pageNumber[pageNumber.length - 2]) {
+            pageNumberList = pageNumber.slice(currentButton - 3, currentButton + 2)
+        } else if (currentButton === pageNumber[pageNumber.length - 2]) {
+            pageNumberList = pageNumber.slice(currentButton - 4, pageNumber[pageNumber.length - 1])
+        } else if (pageNumber.length !== 4) {
+            pageNumberList = pageNumber.slice(currentButton - 5, pageNumber[pageNumber.length - 1])
+        }
+
+        setCurrentPageNumber(pageNumberList)
+        setCurrentPage(currentButton)
+    }, [currentButton, items, itemsPerPage])
 
     return <>
         <div className={styles.wrapper}>
             <button
                 type="button"
-                onClick={() => setCurrentPage(currentPage - 1)}
+                onClick={() => setCurrentButton(currentButton - 1)}
                 className={classNames(styles.prev, { [`${styles.prev__disabled}`]: currentPage === 1 })}
             >
                 <img src={buttonPrev} alt="" />
             </button>
-            {pageNumbers.map(number => {
+            {currentPageNumber.map(number => {
                 return <button type="button"
                     className={classNames(styles.button, { [`${styles.button__active}`]: currentPage === number })}
-                    onClick={() => setCurrentPage(number)}
+                    onClick={() => setCurrentButton(number)}
                     key={number}>
                     {number}
                 </button>
             })}
             <button
                 type="button"
-                onClick={() => setCurrentPage(currentPage + 1)}
-                className={classNames(styles.next, { [`${styles.next__disabled}`]: currentPage === pageNumbers.length })}
+                onClick={() => setCurrentButton(currentButton + 1)}
+                className={classNames(styles.next, { [`${styles.next__disabled}`]: currentPage === pageNumber.length })}
             >
                 <img src={buttonNext} alt="" />
             </button>
